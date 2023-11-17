e6c36f24ccb914ab78ed686a85509bd0490cb2d7
Sawkot Hossen Toha <123503623+shToha901@users.noreply.github.com>
2023-11-18 03:49:59 +0600

    Output 9

---
+++ URI_2755.C
@@ -0,0 +1,14 @@
+// CODE FOR: Output 9.
+
+#include <stdio.h>
+
+int main()
+{
+    // playing with Escape Sequence (Just Print)
+    printf("\"Ro'b'er\tto\\/\"\n");
+    printf("(._.) ( l: ) ( .-. ) ( :l ) (._.)\n");
+    printf("(^_-) (-_-) (-_^)\n");
+    printf("(\"_\") ('.')\n");
+
+    return 0;
+}
