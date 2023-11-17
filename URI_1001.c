f7f81061c19b4aa4a6e7f5fb3a330faba1bc51a9
Sawkot Hossen Toha <123503623+shToha901@users.noreply.github.com>
2023-11-18 03:44:28 +0600

    Extremely Basic

---
+++ URI_1001.c
@@ -0,0 +1,18 @@
+// CODE FOR: Extremly Basic.
+
+#include <stdio.h>
+
+int main()
+{
+    // Taking input of two integer
+    int A, B, X;
+    scanf("%d %d", &A, &B);
+
+    // Calculating sum
+    X = A + B;
+
+    // Printing output
+    printf("X = %d\n", X);
+
+    return 0;
+}
