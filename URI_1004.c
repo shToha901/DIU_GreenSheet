02b673f6d5478f14dd1bdf8a0d3a90585c8a2890
Sawkot Hossen Toha <123503623+shToha901@users.noreply.github.com>
2023-11-18 03:44:57 +0600

    Simple Product

---
+++ URI_1004.c
@@ -0,0 +1,19 @@
+// CODE FOR: Simple Product.
+
+#include <stdio.h>
+
+int main()
+{
+    // Taking input of two integer
+    int a, b;
+    scanf("%d %d", &a, &b);
+
+    // Calculation for product
+    int PROD;
+    PROD = a * b;
+
+    // Printing output
+    printf("PROD = %d\n", PROD);
+
+    return 0;
+}
