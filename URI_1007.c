4bcd962cd72011332b465065a19604ce3a81a9e2
Sawkot Hossen Toha <123503623+shToha901@users.noreply.github.com>
2023-11-18 03:45:40 +0600

    Difference

---
+++ URI_1007.c
@@ -0,0 +1,15 @@
+// CODE FOR: Difference.
+
+#include <stdio.h>
+
+int main()
+{
+    // Taking input of 4 integer
+    int a, b, c, d;
+    scanf("%d %d %d %d", &a, &b, &c, &d);
+
+    // Calculation for difference and printing the output
+    printf("DIFERENCA = %d\n", a * b - c * d);
+
+    return 0;
+}
